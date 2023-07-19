19e5d49dd0adcd56b95d2ac0a727b46c9e66b544
alanbarco <alaxbarc@espol.edu.ec>
2023-07-19 18:11:01 -0500

    main using the functions

--- src/main.py
+++ src/main.py
@@ -74,3 +74,67 @@ def calc_meal_surcharge(meal):
         meal_surcharge = calc_meal_total_cost * 0.05
 
     return meal_surcharge
+
+def validate_amount(client_order):
+    total_amount = 0
+    for amount in client_order.values():
+        total_amount += amount
+    if total_amount < 0:
+        client_order.pop_item()
+        raise ValueError("ERROR: La cantidad debe ser positiva!")
+    if (total_amount > 100):
+        client_order.pop_item()
+        raise ValueError("ERROR: La cantidad debe ser menor a 100!")
+
+
+def print_order(client_order):
+    print("\n-----ORDEN DEL CLIENTE-----")
+    print("Cantidad | Plato")
+    for meals, amount in client_order.items():
+        print(amount, " ", meals)
+
+
+def print_menu():
+    pass
+
+
+if __name__ == '_main_':
+    i_meal = 0
+    while i_meal != -1:
+        print("=== MENU ===")
+
+        for i in range(len(meals)):
+            print(f"{i}. {meals[i]}: ${prices[i]}")
+        print("-1. Salir")
+
+        i_meal = int(input("\nSelecione su comida: "))
+
+        while True:
+            try:
+                if (i_meal != -1):
+                    meal_qty = int(
+                        input("Ingrese cantidad deseada (max. 100): "))
+                selected_meal = meals[i_meal]
+
+                if selected_meal not in client_order:
+                    client_order[selected_meal] = 0
+
+                client_order[selected_meal] += meal_qty
+                validate_amount(client_order)
+            except ValueError as e:
+                print(e)
+            else:
+                break
+
+        # selected_meal = meals[i_meal]
+
+        # if selected_meal not in client_order:
+        #     client_order[selected_meal] = 0
+
+        # client_order[selected_meal] += meal_qty
+
+        # print(client_meals)
+        # print(cantidades_cliente)
+        print_order(client_order)
+       # print(
+        #  f"Costo final: {calc_cost(client_meals, cantidades_cliente)}")
